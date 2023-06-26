2f381fd3a7fc95b64fcbcc3cd797e59ef52274b0
tugrulkara <74429693+tugrulkara@users.noreply.github.com>
2023-06-26 16:35:18 +0300

    editing navHost and add navArguments

--- app/src/main/java/com/tugrulkara/marvelcompose/presentation/MainActivity.kt
+++ app/src/main/java/com/tugrulkara/marvelcompose/presentation/MainActivity.kt
@@ -1,18 +1,18 @@
 package com.tugrulkara.marvelcompose.presentation
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.tugrulkara.marvelcompose.presentation.marvel_char.views.MarvelCharScreen
 import com.tugrulkara.marvelcompose.presentation.marvel_series.views.MarvelSeriesScreen
 import com.tugrulkara.marvelcompose.presentation.ui.theme.MarvelComposeTheme
@@ -21,23 +21,37 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MarvelComposeTheme {
-                // A surface container using the 'background' color from the theme
+                val navController= rememberNavController()
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.primary
                 ) {
-                    val navController= rememberNavController()
+
                     NavHost(navController = navController, startDestination = Screen.MarvelCharScreen.root ){
                         composable(Screen.MarvelCharScreen.root){
                             MarvelCharScreen(navController = navController)
                         }
-                        composable(
-                            route = "${Screen.MarvelSeriesScreen.root}/{${CHAR_ID}}"){
-                            MarvelSeriesScreen()
+                        composable(route = "${Screen.MarvelSeriesScreen.root}/{${CHAR_ID}}/{charName}/{charDescription}/{charImage}"){
+                            val charName= remember {
+                                it.arguments?.getString("charName")
+                            }
+                            val charDescription= remember {
+                                it.arguments?.getString("charDescription")
+                            }
+                            val charImage= remember {
+                                it.arguments?.getString("charImage")
+                            }
+                            MarvelSeriesScreen(
+                                charName ?: "",
+                                charDescription?: "",
+                                charImage?: "",
+                                navController)
                         }
                     }
 
